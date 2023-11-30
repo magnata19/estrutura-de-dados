59bfa6e0a4e466eb29d5669dc7bdf278d419358b
Davidson <dehpacifico@gmail.com>
2023-11-30 20:39:51 -0300

    criando classe pilha e metodos para alterar os nós da pilha

---
+++ src/one/digitalinnovation/Pilha.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation;
+
+public class Pilha {
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    //método para adicionar um no na pilha
+    public void push(No novoNo){
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar);
+    }
+
+    //método que exclui o ultimo/primeiro elemento da pilha
+    public No pop(){
+     if(!this.isEmpty()){
+         No noPoped = refNoEntradaPilha;
+         refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+         return noPoped;
+        }   return null;
+    }
+
+    //método que chama o no do topo sem excluir
+    public No top(){
+        return refNoEntradaPilha;
+    }
+
+    public boolean isEmpty() {
+      return  refNoEntradaPilha == null ? true : false;
+    }
+}
