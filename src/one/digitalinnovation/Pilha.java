8c7f845759828ef9283cd73f2ba5b211ad7ac7a5
Davidson <dehpacifico@gmail.com>
2023-11-30 22:05:24 -0300

    executando métodos para alterar nós em uma pilha

--- src/one/digitalinnovation/Pilha.java
+++ src/one/digitalinnovation/Pilha.java
@@ -31,4 +31,23 @@ public class Pilha {
     public boolean isEmpty() {
       return  refNoEntradaPilha == null ? true : false;
     }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "----------\n";
+        stringRetorno +="       Pilha\n";
+        stringRetorno += "----------\n";
+
+        No noAuxiliar = refNoEntradaPilha;
+        while(true){
+            if(noAuxiliar != null) {
+                stringRetorno += "[No {dado = " + noAuxiliar.getDado() + "}]\n";
+                noAuxiliar = noAuxiliar.getRefNo();
+            } else {
+                break;
+            }
+        }
+        stringRetorno += "==========";
+        return stringRetorno;
+    }
 }
